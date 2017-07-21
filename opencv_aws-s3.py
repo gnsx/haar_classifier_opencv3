b5f2f85ba6c2c84369e5ddf7e2dea17c8b44654c
gnsx <kevin.dsouza3691@gmail.com>
2017-07-21 09:13:46 +0530

    Added threads and exit statements

--- opencv_aws-s3.py
+++ opencv_aws-s3.py
@@ -10,6 +10,10 @@ USAGE:
 # Python 2/3 compatibility
 from __future__ import print_function
 
+import time
+import Queue
+import threading
+import requests
 import numpy as np
 import cv2
 
@@ -19,7 +23,7 @@ from common import clock, draw_str
 
 
 def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=2.5, minNeighbors=8, minSize=(30, 30),
+    rects = cascade.detectMultiScale(img, scaleFactor=4.0, minNeighbors=8, minSize=(48, 48),
                                      flags=cv2.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
         return []
@@ -30,7 +34,27 @@ def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
-if __name__ == '__main__':
+kill_timer = True
+content_type = 'image/jpeg'
+headers = {'content-type': content_type}
+
+def push_to_cloud():
+	print ("Entered Queue")
+	while kill_timer:
+		print ("Size of Queue : " + str(q.qsize()))
+		while not q.empty():
+			temp_img = q.get()
+			_, img_encoded = cv2.imencode('.jpg', temp_img)
+			response = requests.post("http://172.16.0.109:2777/toll/rek", data=img_encoded.tostring(), headers=headers)
+			print ("sent data out data")
+			print (response)
+		time.sleep(1)
+	print ("Exiting push_to_cloud_thread")
+	return
+		
+q = Queue.Queue()
+		
+def main():
 	import sys, getopt
 	print(__doc__)
 	
@@ -41,26 +65,54 @@ if __name__ == '__main__':
 		video_src = 0
 	args = dict(args)
 	cascade_fn = args.get('--cascade', "car_cascade.xml")
-	
+		
 	cascade = cv2.CascadeClassifier(cascade_fn)
-	cam = cv2.VideoCapture("rtsp://admin:admin@172.16.20.20:554/snl/live/1/1")
-	i = 0
-	while True:
-		ret, img = cam.read()
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		gray = cv2.equalizeHist(gray)
-		t = clock()
-		rects = detect(gray, cascade)
-		#print (str(len(rects)))
-		vis = img.copy()
-		draw_rects(vis, rects, (0, 255, 0))
-		dt = clock() - t
+	while kill_timer:
+		print ("Opening Stream from rtsp")
+		cam = cv2.VideoCapture("rtsp://admin:admin@172.16.20.20:554/snl/live/1/1")
+		i = 0
+		do_loop = True
+		while do_loop and kill_timer:
+			ret, img = cam.read()
+			try:
+				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+				gray = cv2.equalizeHist(gray)
+				t = clock()
+				rects = detect(gray, cascade)
+				#print (str(len(rects)))
+				vis = img.copy()
+				draw_rects(vis, rects, (0, 255, 0))
+				dt = clock() - t
+				draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+				#print (str(1/dt) + ' ' + str(i) + 'box')
+				if (len(rects)) > 0 :
+					cv2.imwrite(str(i) + 'image.jpg',img)
+					q.put(img)
+					i+=1
+				#cv2.imshow('facedetect', vis)
+				if cv2.waitKey(5) == 27:
+					break
+			except:
+				do_loop = False
+				cv2.destroyAllWindows()
+				cam = None
+				print ("Error")
+				
+	cv2.destroyAllWindows()
+	print ("Out of here")
+		
+if __name__ == '__main__':
 	
-		draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
-		print (str(i) + 'box')
-		if (len(rects)) > 0 :
-			cv2.imwrite(str(i) + 'image.jpg',vis)
-			i+=1
-		if cv2.waitKey(5) == 27:
-			break
-		cv2.destroyAllWindows()
+	kill_timer = True
+	t1 = threading.Thread(target=push_to_cloud)
+	t2 = threading.Thread(target=main)
+	t1.start()
+	t2.start()
+	try:
+		while True:
+			time.sleep(.1)
+	except KeyboardInterrupt:
+		print ("attempting to close threads.")
+		kill_timer = False
+        
+        print ("threads successfully closed")
